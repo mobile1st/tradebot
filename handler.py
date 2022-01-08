31c698e95849fb96eb7de7ea6fadf1a31ef26a84
Rashi Abramson <roshkins@gmail.com>
2022-01-08 13:05:56 -0800

    calculate leverage to not go over

--- handler.py
+++ handler.py
@@ -57,20 +57,11 @@ def trade(event, context):
         web3=None,
     )
 
-    # # Onboard the account.
-    # onboarding_response = client.onboarding.create_user(
-    #     stark_public_key=public_x,
-    #     stark_public_key_y_coordinate=public_y,
-    # )
-    # print('onboarding_response', onboarding_response)
-
-    # Query a private endpoint.
-
     account_response = client.private.get_account(WALLET_ADDRESS).data
     print('account_response', account_response)
     logger.info('account_response', account_response)
     position_id = account_response['account']['positionId']
-    free_collateral = Decimal(account_response['account']['freeCollateral'])
+    equity = Decimal(account_response['account']['equity'])
     user = client.private.get_user().data['user']
     makerFeeRate = user['makerFeeRate']
     takerFeeRate = user['takerFeeRate']
@@ -80,22 +71,34 @@ def trade(event, context):
     logger.debug('marketData data {}'.format(marketData))
     tickSize = marketData['tickSize']
     stepSize = marketData['stepSize']
+    oraclePrice = Decimal(marketData['oraclePrice'])
 
     print('event', message, 'tickSize', tickSize)
-    size = Decimal(message.get('size')).quantize(Decimal(stepSize))
+    orderSize = Decimal(message.get('size')).quantize(Decimal(stepSize))
     price = Decimal(message.get('price')).quantize(Decimal(tickSize))
     maxTxFee = Decimal(message.get('maxTxFee')).quantize(Decimal(stepSize))
     estimatedFeePercent = Decimal(
         max(makerFeeRate, takerFeeRate)).quantize(Decimal(stepSize))
-    estimatedFee = Decimal(estimatedFeePercent * size * price)
+    estimatedFee = Decimal(estimatedFeePercent * orderSize * price)
+
+    eth_position = client.private.get_positions(
+        market=MARKET_ETH_USD,
+        status=POSITION_STATUS_OPEN,
+    ).data['positions'][0]
+    positionSize = Decimal(eth_position['size'])
+
+    leverage = ((abs(positionSize) + abs(orderSize)) * oraclePrice)/equity
+    if leverage > Decimal(1.0):
+        error = {
+            'message': 'Not enough equity to stay out of margin, estimated leverage: {}'.format(leverage)}
+        logger.exception(json.dumps(error))
+        return {'statusCode': 400, 'body': json.dumps(error)}
+
     if estimatedFee > maxTxFee:
-        logger.exception(json.dumps(
-            {'message': 'Max Tx Fee exceeded, {} > {}'.format(estimatedFee, maxTxFee)}))
-        return {'statusCode': 400, 'body': json.dumps({'message': 'Max Tx Fee exceeded, {} > {}'.format(estimatedFee, maxTxFee)})}
-    if size * price + estimatedFee >= free_collateral:
-        logger.exception(json.dumps({'message': 'Free Collateral exceeded, {} > {}'.format(
-            size * price + estimatedFee, free_collateral)}))
-        return {'statusCode': 400, 'body': json.dumps({'message': 'Free Collateral exceeded, {} > {}'.format(estimatedFee, free_collateral)})}
+        error = json.dumps(
+            {'message': 'Max Tx Fee exceeded, {} > {}'.format(estimatedFee, maxTxFee)})
+        logger.exception(error)
+        return {'statusCode': 400, 'body': error}
 
     order_params = {
         'position_id': position_id,
@@ -103,7 +106,7 @@ def trade(event, context):
         'side': ORDER_SIDE_BUY,
         'order_type': ORDER_TYPE_LIMIT,
         'post_only': False,
-        'size': str(size),
+        'size': str(orderSize),
         'price': str(price),
         'limit_fee': str(estimatedFeePercent),
         'expiration_epoch_seconds': time.time() + 120,
@@ -159,32 +162,39 @@ def cost_basis_sell(event, context):
         web3=None,
     )
 
+    marketData = client.public.get_markets(
+        MARKET_ETH_USD).data['markets'][MARKET_ETH_USD]
+    tickSize = Decimal(marketData['tickSize'])
+    stepSize = Decimal(marketData['stepSize'])
+
+    position_response = client.private.get_positions(
+        market=MARKET_ETH_USD,
+        status=POSITION_STATUS_OPEN,
+    ).data['positions'][0]
+    logger.debug(position_response)
+    position_size = Decimal(position_response['size'])
+    entry_price = Decimal(
+        position_response['entryPrice'])
+    cost_basis = (entry_price/position_size).quantize(Decimal(stepSize))
+    print('cost_basis', cost_basis)
+    logger.info('cost_basis' + str(cost_basis))
+
+    indexPrice = Decimal(marketData['indexPrice']).quantize(tickSize)
+
     account_response = client.private.get_account(WALLET_ADDRESS).data
     print('account_response', account_response)
     logger.info('account_response', account_response)
-    cost_basis = Decimal(
-        account_response['account']['entryPrice'])/Decimal(account_response['account']['size'])
-    print('cost_basis', cost_basis)
-    logger.info('cost_basis', cost_basis)
-
-    marketData = client.public.get_markets(
-        MARKET_ETH_USD).data['markets'][MARKET_ETH_USD]
-    tickSize = marketData['tickSize']
-    stepSize = marketData['stepSize']
-    indexPrice = Decimal(marketData['indexPrice']).quantize(tickSize)
-
     position_id = account_response['account']['positionId']
-    quoteBalance = Decimal(account_response['account']['quoteBalance'])
     user = client.private.get_user().data['user']
-    makerFeeRate = user['makerFeeRate']
-    takerFeeRate = user['takerFeeRate']
+    makerFeeRate = Decimal(user['makerFeeRate'])
+    takerFeeRate = Decimal(user['takerFeeRate'])
     estimatedFeePercent = Decimal(
         max(makerFeeRate, takerFeeRate)).quantize(Decimal(stepSize))
 
     # Constants
-    SELL_SIZE = Decimal(0.1)  # ETH
-    MAX_FEE = 1  # Dollar
-    PROFIT_PERCENT = Decimal(1.01)
+    SELL_SIZE = Decimal(0.01).quantize(Decimal(stepSize))  # ETH
+    MAX_FEE = Decimal(1).quantize(Decimal(tickSize))  # Dollar
+    PROFIT_PERCENT = Decimal(1.01).quantize(Decimal('1.00'))
 
     estimatedFee = Decimal(estimatedFeePercent * SELL_SIZE)
     if indexPrice < cost_basis * PROFIT_PERCENT:
@@ -199,9 +209,9 @@ def cost_basis_sell(event, context):
         logger.exception(json.dumps(error))
         return {'statusCode': 400, 'body': json.dumps(error)}
 
-    if SELL_SIZE + estimatedFee >= quoteBalance:
-        error = {'message': 'Quote balance exceeded, {} > {}'.format(
-            SELL_SIZE * indexPrice + estimatedFee, quoteBalance)}
+    if SELL_SIZE + estimatedFee >= position_size:
+        error = {'message': 'Position size exceeded, {} > {}'.format(
+            SELL_SIZE + estimatedFee, position_size)}
         logger.exception(json.dumps(error))
         return {'statusCode': 400, 'body': json.dumps(error)}
 
